92cee3fb89cf7e3e90886933e8ed4147c8227b3d
teffalump <siv@riseup.net>
2012-08-04 21:57:02 -0700

    started adding the mongo backend

--- display.js
+++ display.js
@@ -1,11 +1,28 @@
 // show all the statistics, etc
 
-module.exports = function displaySetup()
+module.exports = function displaySetup(dBInstance, questColl)
     {
-    return function displayHandle(req, res, next)
-        {
-            res.writeHead(200);
-            res.end('YAY');
-            return;
-        };
+        var db = dBInstance,
+            quest = questColl;
+        return function displayHandle(req, res, next)
+            {
+                db.open(function (err, db)
+                        {
+                            if (!err)
+                                {
+                                    db.collection(quest, function (err, collection)
+                                            {
+                                                assert.ok(err != null, quest + " does not exist");
+
+                                                // need to excise unneeded info
+                                                // in future
+                                                res.writeHead(200);
+                                                var stream = collection.find().streamRecords();
+                                                stream.on("data", function(item) {res.write(item)});
+                                                stream.on("end", function() {res.end()});
+                                                return;
+                                            };
+                                };
+                        };
+            };
     };
--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 // main script
 
-// the connect app, with all its middleware
-var connect = require('connect');
-var login = require('./login.js');
-var logout = require('./logout.js');
-var crud = require('./crud.js');
-var auth = require('./auth.js');
-var display = require('./display.js');
+// superuser access for authentication
+// will push this to the db eventually
+var authMap = {
+    "user":'foo',
+    "password":'bar'
+    };
 
-// mapping of bool = authentication needed? to uri
+// authenticatin map of app
+//     resource => auth needed?
 var urlMap = {
     "/": false,
     "/login": false,
     "/logout": true,
     "/crud": true
     };
-// superuser info
-var authMap = {
-    "user":'foo',
-    "password":'bar'
-    };
 
+// the connect prepreqs
+var connect = require('connect'),
+    login = require('./login.js'),
+    logout = require('./logout.js'),
+    crud = require('./crud.js'),
+    auth = require('./auth.js'),
+    display = require('./display.js');
+
+// the database connection and info
+var dbName = 'test',
+    userColl = 'users',
+    questColl = 'questionnaires';
+
+var mongo = require('mongodb'),
+    Server = mongo.Server,
+    Db = mongo.Db;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true}),
+    db = new Db(dbName, server);
+
+// the app with middleware
 var app = connect()
         .use(connect.logger({ format: ':method :url' }))
         .use(connect.cookieParser())
@@ -29,9 +45,8 @@ var app = connect()
         .use(auth(urlMap))
         .use('/login', login(authMap))
         .use('/logout', logout())
-        .use('/crud', crud())
-        .use(display());
-
+        .use('/crud', crud(db))
+        .use(display(db, questColl));
 
 // https options -- need key and cert
 //var https = require('https');
